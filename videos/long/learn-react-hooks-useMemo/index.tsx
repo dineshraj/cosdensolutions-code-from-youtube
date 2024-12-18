ffadb429f51a4c598324d5baa65fa56ec5c18ab1
dineshraj <dineshraj.goomany@gmail.com>
2024-12-18 15:15:33 +0000

    correction

--- videos/long/learn-react-hooks-useMemo/index.tsx
+++ videos/long/learn-react-hooks-useMemo/index.tsx
@@ -6,9 +6,15 @@ interface DemoProps {}
 
 
 /*
- hypothesis: so every time the count changes it causes a rerender
- which means that initialItems is re-executed
- which is expensive because of the 29m+ array
+Hypothesis: 
+  so every time the count changes it causes a rerender
+  which means that initialItems is re-executed
+  which is expensive because of the 29m+ array
+
+WRONG
+  initialItems in use state is stored in state so is only called once
+  The issue is that selectedItem keeps being called with a find on a 29m+
+  array each time the value is clicked, which is expensive
 */
 
 function Demo({}: DemoProps) {
