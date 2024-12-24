3f7d72deec73a0fdcc85d27567b364c0e44d9631
dineshraj <dineshraj.goomany@gmail.com>
2024-12-24 15:50:55 +0000

    remove useRef for implementing myself

--- videos/long/learn-react-hooks-useRef/index.tsx
+++ videos/long/learn-react-hooks-useRef/index.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 interface DemoProps {}
 
 export default function Demo({}: DemoProps) {
   const [count, setCount] = useState(0);
-  const countRef = useRef(0);
 
   const handleIncrement = () => {
     setCount(count + 1);
-    countRef.current++;
 
     console.log("State:", count);
-    console.log("Ref:", countRef.current);
   };
 
   return (
@@ -23,15 +20,10 @@ export default function Demo({}: DemoProps) {
 }
 
 export function Demo2({}: DemoProps) {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  useEffect(() => {
-    inputRef.current?.focus();
-  }, []);
 
   return (
     <div className="tutorial">
-      <input ref={inputRef} type="text" placeholder="Type something..." />
+      <input type="text" placeholder="Type something..." />
     </div>
   );
 }
