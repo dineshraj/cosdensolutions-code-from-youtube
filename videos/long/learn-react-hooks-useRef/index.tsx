baa21edf9eeac6538a765674b00fd7326f07253e
dineshraj <dineshraj.goomany@gmail.com>
2024-12-24 16:03:44 +0000

    add useRef code

--- videos/long/learn-react-hooks-useRef/index.tsx
+++ videos/long/learn-react-hooks-useRef/index.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 interface DemoProps {}
 
 export default function Demo({}: DemoProps) {
   const [count, setCount] = useState(0);
+  const countRef = useRef(0)
 
   const handleIncrement = () => {
     setCount(count + 1);
+    countRef.current++;
 
     console.log("State:", count);
+    console.log('Ref', countRef.current);
   };
 
   return (
@@ -19,11 +22,16 @@ export default function Demo({}: DemoProps) {
   );
 }
 
-export function Demo2({}: DemoProps) {
+export function Demo2({ }: DemoProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, []);
 
   return (
     <div className="tutorial">
-      <input type="text" placeholder="Type something..." />
+      <input ref={inputRef} type="text" placeholder="Type something..." />
     </div>
   );
 }
